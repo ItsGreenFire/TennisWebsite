266f2365f03007ff0ac798511c622ad1a05e527a
lajollalerner <lajollalerner@gmail.com>
2022-08-20 18:25:29 -0700

    main

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ let infoTable = document.getElementById("info-table");
 let shown = true;
 
 let date =  new Date();
-console.log(date);
 
 currentDate = date.getFullYear()+"-"+padzero(date.getMonth()+1)+"-"+padzero(date.getDate());
 currentTime = padzero(date.getHours())+":"+padzero(date.getMinutes());
-console.log(currentTime,currentDate)
 
 document.getElementById('date-select').value = currentDate;
 document.getElementById('time-select').value = currentTime;
 
 let events = [];
 
+
 function padzero(n) {
   return ("0"+n).slice(-2)
 }
+
+
 function geteventinfo() {
   let name = document.getElementById("name-select");
   let time = document.getElementById("time-select");
   let date = document.getElementById("date-select");
+  let duration = document.getElementById("minute-select");
   name = name.options[name.selectedIndex].text;
   time = time.value;
   date = date.value;
+  duration = duration.value
 
-  return {"eDate": date, "eTime": time, "ePerson": name};
+  return {"eDate": date, "eTime": time, "eDuration": duration, "ePerson": name};
 }
 
 function addEvent(e) {
@@ -39,6 +42,7 @@ function add_to_table() {
     "    <tr>" +
     "        <th>Date</th>" +
     "        <th>Time</th>" +
+    "        <th>Dur</th>" +
     "        <th>Name</th>" +
     "    </tr>";
 
@@ -46,58 +50,97 @@ function add_to_table() {
     let row = infoTable.insertRow(1);
     let dCell = row.insertCell(0);
     let tCell = row.insertCell(1);
-    let pCell = row.insertCell(2);
+    let lCell = row.insertCell(2)
+    let pCell = row.insertCell(3);
     dCell.innerHTML = e.eDate.substring(5);
     tCell.innerHTML = e.eTime;
+    lCell.innerHTML = e.eDuration;
     pCell.innerHTML = e.ePerson;
 
   });
 }
 
+function size_table() {
+  document.addEventListener("DOMContentLoaded", function () {
+    let table = document.getElementById('info-div');
+    let topArea = document.getElementById('topArea');
+    let size = screen.height - topArea.offsetHeight;
+    table.style.height = size.toString() + 'px';
+    console.log(screen.height - topArea.offsetHeight, screen.height, topArea.offsetHeight);
+  });
+}
 
 function hide() {
-  let topArea = document.getElementById('topArea')
-  let closeButton = document.getElementById('hide')
+  let topArea = document.getElementById('topArea');
+  let closeButton = document.getElementById('hide');
+  let openClose = document.getElementById('open-close');
 
   const openSlide = [
     { transform: 'translateY(0)' },
-    { transform: 'translateY(-650px)' }];
+    { transform: 'translateY(-1280px)' }];
 
   const closeSlide = [
-    { transform: 'translateY(-550px)' },
-    { transform: 'translateY(0)' }];
+    { transform: 'translateY(-380px)' },
+    { transform: 'translateY(900px)' }];
+
+  const rotate = [
+    { transform: 'rotate(0)' },
+    { transform: 'rotate(180deg)' }];
 
   const timing = {
-    duration: 350,
+    duration: 800,
     iterations: 1 };
 
+  const arrowTiming = {
+    duration : 100,
+    iterations: 8 };
+
   if (shown) {
-    document.getElementById('hide').innerHTML = 'SHOW';
     topArea.animate(openSlide, timing);
     closeButton.animate(openSlide, timing);
-    setTimeout(function() {  topArea.style.marginTop = "-650px" }, 350);
-    document.getElementById('save-button').style.display = 'none'
-
+    openClose.animate(rotate, arrowTiming);
+    setTimeout(function() {
+      topArea.style.marginTop = "-900px";
+      openClose.innerHTML = 'arrow_circle_down';
+      document.getElementById('save-button').style.display = 'none';
+      }, 800);
   } else {
-    document.getElementById('hide').innerHTML = 'HIDE';
     topArea.animate(closeSlide, timing);
     closeButton.animate(closeSlide, timing);
-    setTimeout(function() {  topArea.style.marginTop = "0" }, 350);
-    document.getElementById('save-button').style.display = 'inline'
+    openClose.animate(rotate, arrowTiming);
+    document.getElementById('save-button').style.display = 'inline';
+    setTimeout(function() {
+      topArea.style.marginTop = "0";
+      openClose.innerHTML = 'arrow_circle_up';
+      }, 800);
   }
   shown = !shown;
+  size_table();
 }
 
 function randomColor() {
-  let colors = ['#ffffff', '#ffde9d', '#c0ffc2'];
-  let random_color = colors[Math.floor(Math.random() * colors.length)];
+  let themes = [
+    {primary: '#60D394', secondary: '#AAF683', tertiary: '#FFD97D', quaternary: '#EE6055', quinternary: '#FF9B85'},
+    {primary: '#6c584c', secondary: '#f0ead2', tertiary: '#f0ead2', quaternary: '#a98467', quinternary: '#dde5b6'},
+    {primary: '#283618', secondary: '#fefae0', tertiary: '#dda15e', quaternary: '#bc6c25', quinternary: '#606c38'}
+  ]
 
-  document.getElementById('topArea').style.backgroundColor = random_color;
-  document.getElementById('name-select').style.backgroundColor = random_color;
-  document.getElementById('date-select').style.backgroundColor = random_color;
-  document.getElementById('time-select').style.backgroundColor = random_color;
-  document.getElementById('hide').style.backgroundColor = random_color;
-  document.getElementById('hide').style.borderTopColor = random_color;
+  let random_theme = themes[Math.floor(Math.random() * themes.length)];
+
+  document.getElementById('topArea').style.backgroundColor = random_theme.primary;
+  document.getElementById('save-button').style.backgroundColor = random_theme.secondary;
+  document.getElementById('save-button').style.color = random_theme.quaternary;
+  document.getElementById('name-select').style.backgroundColor = random_theme.primary;
+  document.getElementById('name-select').style.color = random_theme.tertiary;
+  document.getElementById('date-select').style.backgroundColor = random_theme.primary;
+  document.getElementById('date-select').style.color = random_theme.tertiary;
+  document.getElementById('time-select').style.backgroundColor = random_theme.primary;
+  document.getElementById('time-select').style.color = random_theme.tertiary;
+  document.getElementById('minute-select').style.backgroundColor = random_theme.primary;
+  document.getElementById('minute-select').style.color = random_theme.tertiary;
+  document.getElementById('hide').style.backgroundColor = random_theme.primary;
+  document.getElementById('hide').style.borderTopColor = random_theme.primary;
+  document.body.style.backgroundColor = random_theme.quinternary
 }
 
-//randomColor()
+randomColor()
